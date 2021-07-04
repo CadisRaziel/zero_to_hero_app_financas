880158cf5e4943af441dfeb7df6dc9c6aa3de3f9
CadisRaziel <vitorcsharp@hotmail.com>
2021-07-04 15:26:18 -0300

    realizando a tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'core/app_widget.dart';
-
 void main() {
-  runApp(MyApp());
+  //Para evitar que rotacione a tela !!!
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+    .then((_) {
+      runApp(MyApp());
+    });
 }
 
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zero_to_hero/shared/theme/app_text_Style.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -23,12 +24,108 @@ class _LoginPageState extends State<LoginPage> {
             alignment: Alignment.bottomCenter,
             child: Container(
               width: size.width,
-              height: size.height * 0.6,
+              height: size.height * 0.70,
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(65),
-                      topRight: Radius.circular(65))),
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40))),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40, left: 32, right: 32),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Bem Vindo',
+                        style: TextStyles.titleHome,
+                      ),
+                      Text(
+                        'Mantenha suas contas em dia',
+                        style: TextStyles.titleHome,
+                      ),
+                      SizedBox(height: 40),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 6, right: 250),
+                        child: Text(
+                          'Email:',
+                          style: TextStyles.titleHome,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16, right: 16),
+                        child: TextField(
+                          decoration:
+                              InputDecoration(border: OutlineInputBorder()),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 12, right: 250),
+                        child: Text(
+                          'Senha:',
+                          style: TextStyles.titleHome,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16, right: 16),
+                        child: TextField(
+                          decoration:
+                              InputDecoration(border: OutlineInputBorder()),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.teal,
+                                    onPrimary: Colors.white,
+                                    shadowColor: Colors.lightGreenAccent,
+                                    elevation: 5,
+                                  ),
+                                  child: Text(
+                                    'Entrar',
+                                    style: TextStyles.titleHome,
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: Text('Esqueci minha senha',
+                                  style: TextStyles.titlebutton),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: Text('Criar conta',
+                                  style: TextStyles.titlebutton),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           )
         ],
